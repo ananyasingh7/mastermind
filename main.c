56d39753ef771a00db3933820ea8ecef746f059b
Ananya Singh <AnanyaSingh@andres-mbp.resource.campus.njit.edu>
2019-01-31 17:54:13 -0500

    setting up basic program

---
+++ main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(){
+	printf("Available Colors: (B)lue (G)reen (O)range (P)urple (R)ed (Y)ellow\n");
+
+        int guesses = 12;
+	bool gameOver = false; 
+
+
+	while(!gameOver){
+		printf("testing123");
+		gameOver = true;
+		printf("%d",guesses);
+	}
+
+	
+
+
+
+	return 0;
+}
