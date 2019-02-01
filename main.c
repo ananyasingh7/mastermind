cf6dbb9401473802d1af88a2fa03f48c089ecfac
Ananya Singh <AnanyaSingh@Andres-MacBook-Pro.local>
2019-02-01 10:49:45 -0500

    update

--- main.c
+++ main.c
@@ -3,17 +3,30 @@
 #include<string.h>
 #include<stdlib.h>
 
-int main(){
+int correct(char* word, char* correctWord){
+	for(int i = 0; i<4; ++i){
+		
+	}
+}
+
+
+
+
+int main(int argc, char* argv[]){
 	printf("Available Colors: (B)lue (G)reen (O)range (P)urple (R)ed (Y)ellow\n");
 
-        int guesses = 12;
+    int guesses = 12;
 	bool gameOver = false; 
-
+	char codemaker[256];
+	strcpy(codemaker, argv[1]);
 
 	while(!gameOver){
-		printf("testing123");
 		gameOver = true;
-		printf("%d",guesses);
+		printf("No. guesses left: %d\n",guesses + argc);
+		char guess[256];
+		printf("Enter your guess: ");
+		scanf("%s",guess);
+		
 	}
 
 	
